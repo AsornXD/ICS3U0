c3550be4ee99e3e4ad1eb52032a331974942b038
AsornXD <45113995+AsornXD@users.noreply.github.com>
2019-09-28 14:37:38 -0400

    aaa

---
+++ Array Challenges/Challenge1.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class Challenge1 {
+    public static void main(String[] args) {
+        String[] names;
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter the first names of 8 friends (Press enter to move onto the next friend):");
+        names = new String[] {input.nextLine(),input.nextLine(),input.nextLine(),input.nextLine(),input.nextLine(),input.nextLine(),input.nextLine(),input.nextLine()};
+        int maxLength = Integer.MIN_VALUE;
+        String maxWord = "";
+        for (int i = 0; i < 8; i++)
+        {
+            if (names[i].length() > maxLength){
+                maxLength = names[i].length();
+                maxWord = names[i];
+            }
+        }
+        System.out.println(maxWord + " has the longest name with a length of " + maxLength + " letters.");
+    }
+}
---
+++ Array Challenges/Challenge2.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class Challenge2 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int[] sales;
+        System.out.println("Enter the sales for each month of the year (January - December):");
+        sales = new int[] {Integer.parseInt(input.nextLine()),Integer.parseInt(input.nextLine()),Integer.parseInt(input.nextLine()),Integer.parseInt(input.nextLine()),Integer.parseInt(input.nextLine()),Integer.parseInt(input.nextLine()),Integer.parseInt(input.nextLine()),Integer.parseInt(input.nextLine()),Integer.parseInt(input.nextLine()),Integer.parseInt(input.nextLine()),Integer.parseInt(input.nextLine()),Integer.parseInt(input.nextLine())};
+        int totalSales = 0;
+        for (int i = 0; i < 12; i++){
+            totalSales+=sales[i];
+        }
+        System.out.println("The total sales were " + totalSales + " units.");
+        System.out.println("Sales have grown by " + (sales[11] - sales[0]) + " units over the year.");
+    }
+}
